9981965847956b1d06bb92df29593c48215aed7f
ELotfi <59686647+ELotfi@users.noreply.github.com>
2025-07-09 18:14:59 +0200

    Update biencoder_dataloader.py

--- src/loaders/biencoder_dataloader.py
+++ src/loaders/biencoder_dataloader.py
@@ -155,17 +155,21 @@ class SyntRetDataLoader:
     def _get_transformed_datasets(self) -> Tuple:
         #raw_dataset = load_dataset('json', data_files=self.args.train_file)['train']
         raw_dataset = load_dataset('Ehsanl/SynRet', data_files=self.args.train_file)['train']
+        if self.args.train_tasks != 'all':
+            tasks = self.args.train_tasks.strip().split('+')
+            raw_dataset = raw_dataset.filter(lambda x: x['task_type'] in tasks)
         if self.args.max_train_samples is not None:
             raw_dataset = raw_dataset.select(range(self.args.max_train_samples))
         
-        propmts = {'q': 'query: ', 'p':'passage: '}
+        propmts = {'q':self.args.q_prompt, 'p':self.args.p_prompt}
         def _add_prompts(sample):
             sample['q'] = propmts['q'] + sample['q']
             sample['pos'] = propmts['p'] + sample['pos']
             sample['neg'] = propmts['p'] + sample['neg']
             return sample
         
-        raw_dataset = raw_dataset.map(_add_prompts)
+        if self.args.add_prompts:
+            raw_dataset = raw_dataset.map(_add_prompts)
 	    # Log a few random samples from the training set:
         for index in random.sample(range(len(raw_dataset)), 3):
             logger.info(f"Sample {index} of the training set: {raw_dataset[index]}.")
